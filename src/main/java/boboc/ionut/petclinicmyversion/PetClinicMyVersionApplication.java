1f48ffd20879dbf8f881048f042d61b45308141a
JB <ionut.m.boboc@gmail.com>
2019-11-15 16:14:29 +0200

    Initial project add

---
+++ src/main/java/boboc/ionut/petclinicmyversion/PetClinicMyVersionApplication.java
@@ -0,0 +1,13 @@
+package boboc.ionut.petclinicmyversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicMyVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicMyVersionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boboc/ionut/petclinicmyversion/PetClinicMyVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package boboc.ionut.petclinicmyversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicMyVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
